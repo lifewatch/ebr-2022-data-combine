86d584bb7b63c4e8bcf2de8ac55b310c46bfc5b0
salvafern <salvador.fernandez@vliz.be>
2022-05-13 14:49:27 +0200

    continue exercise 4 #2

--- src/exercises/04_combine.R
+++ src/exercises/04_combine.R
@@ -4,6 +4,21 @@ library(sf)
 
 df1 <- head(df)
 
+
+#---------------------------------------------------------------------------------------------
+# Exercise 4.1.: 
+#   - Join seabed data with the dataset
+#
+#---------------------------------------------------------------------------------------------
+
 test <- st_join(df1, seabed_layers)
 
-test2 <- raster::extract()
+#---------------------------------------------------------------------------------------------
+# Exercise 4.2.: 
+#   - Extract temperature data from Bio-Oracle layers
+#
+#---------------------------------------------------------------------------------------------
+
+df1$temperature <- extract(layers$BO22_tempmean_ss, sf::as_Spatial(df1))
+df1$temperature_2050 <- extract(layers$BO22_RCP85_2050_tempmean_ss, sf::as_Spatial(df1))
+df1$temperature_2100 <- extract(layers$BO22_RCP85_2100_tempmean_ss, sf::as_Spatial(df1))
